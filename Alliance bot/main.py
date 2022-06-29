0d3195f1a5439538681f3eb77c5189394eac3caa
Lordir <09mmnn@gmail.com>
2022-06-29 13:08:16 +0700

    add command "start"

--- Alliance bot/main.py
+++ Alliance bot/main.py
@@ -1,7 +1,17 @@
 import telebot
 
-from token import token
+from token_telegram import token
 
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        bot.send_message(message.chat.id, "Hello")
+
+    bot.polling()
+
+
+if __name__ == '__main__':
+    telegram_bot(token)
